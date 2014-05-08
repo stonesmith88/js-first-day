abfabe7ce6e23d50aacfa0be545cb33cb69c578e
stone smith <stonesmith88@yahoo.com>
2014-05-08 07:45:52 -0400

    first day of js

---
+++ main.js
@@ -0,0 +1,24 @@
+// main.js
+
+function square (x) {
+	return x * x;
+}
+// doggy
+var dog = {
+	legs: 4,
+	bark: function(){
+		return "woof!;"
+	}
+}
+
+// grr
+console.log("my dog has", dog.legs, "legs.")
+
+// money
+var balance = 10.97;
+
+  if (balance<10 ) {
+	console.log(ballance-5);
+} else {
+	console.log(balance);
+}
